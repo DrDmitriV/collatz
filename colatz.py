a36421a081d152af9034d054c19a275a25f7479e
DrDmitriV <101051727+DrDmitriV@users.noreply.github.com>
2022-03-06 15:57:07 +0200

    Add files via upload

---
+++ colatz.py
@@ -0,0 +1,12 @@
+n = int(input())
+steps = 0
+
+while n != 1:
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = 3 * n + 1
+    steps += 1
+    print(int(n))
+
+print('\n Your number has reached zero in ', steps, 'tries')
